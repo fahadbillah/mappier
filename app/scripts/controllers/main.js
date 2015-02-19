d86a6ca894a03e8d17637b144299e6e7497b22e3
fahadbillah <fahadbillah@yahoo.com>
2015-02-19 09:48:44 +0600

    marker with time refresh added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the mappierApp
  */
  angular.module('mappierApp')
- .controller('MainCtrl',['$scope', 'uiGmapGoogleMapApi', function ($scope, uiGmapGoogleMapApi) {
+ .controller('MainCtrl',['$scope','$timeout','uiGmapGoogleMapApi', function ($scope,$timeout,uiGmapGoogleMapApi) {
      // Do stuff with your $scope.
      // Note: Some of the directives require at least something to be defined originally!
      // e.g. $scope.markers = []
@@ -16,10 +16,45 @@
      // uiGmapGoogleMapApi is a promise.
      // The "then" callback function provides the google.maps object.
 
-     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+     var i = 0;
 
-     
-     uiGmapGoogleMapApi.then(function(maps) {
-     	console.log(maps);
-     });
+     var getCurrentPosition = function() {
+     	navigator.geolocation.getCurrentPosition(function(position){
+     		console.log(position.coords);
+
+     		var latitude = position.coords.latitude + i;
+     		var longitude = position.coords.longitude + i;
+
+     		$scope.map = { center: { latitude: latitude, longitude: longitude}, zoom: 18 };
+     		$scope.marker = {
+     			id: 1,
+     			coords: null
+     		};
+     		$scope.marker.coords = {
+     			accuracy: 9295,
+     			altitude: null,
+     			altitudeAccuracy: null,
+     			heading: null,
+     			latitude: latitude,
+     			longitude: longitude,
+     			speed: null
+     		};
+     		// $scope.marker.coords.latitude = position.coords.latitude;
+     		// $scope.marker.coords.longitude = position.coords.longitude;
+
+     		console.log($scope.map);
+
+
+
+
+     		uiGmapGoogleMapApi.then(function(maps) {
+     			console.log(maps);
+     		});
+     		i+= 0.0001;
+     	})
+
+     	$timeout(getCurrentPosition, 1000);
+
+     }
+     getCurrentPosition();
  }]);
